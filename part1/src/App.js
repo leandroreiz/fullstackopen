8bde7744fa7976e6e95b5590896404716eb0363e
Leandro Reis <leandro.reiz@gmail.com>
2022-08-19 21:14:31 +0100

    Simple state management

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,23 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+import { useState } from 'react';
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
-  const name = 'Peter';
-  const age = 10;
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </>
   );
 };
 

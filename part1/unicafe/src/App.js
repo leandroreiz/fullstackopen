42a48bc8e9b4c351d8ae56732e067cb0552cbbde
Leandro Reis <leandro.reiz@gmail.com>
2022-08-19 23:56:01 +0100

    Exercise 1.8-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,6 +2,38 @@ import { useState } from 'react';
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ feedback: { good, neutral, bad, all, average } }) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      {!good && !neutral && !bad ? (
+        <div>No feedback given</div>
+      ) : (
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={(average / all).toFixed(1)} />
+            <StatisticLine
+              text="positive"
+              value={((good / all) * 100).toFixed(1).toString().concat('%')}
+            />
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -33,13 +65,7 @@ const App = () => {
       <Button onClick={handleNeutral} text="neutral" />
       <Button onClick={handleBad} text="bad" />
 
-      <h1>statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {average ? average / all : 0}</div>
-      <div>positive {good ? (good / all) * 100 : 0} %</div>
+      <Statistics feedback={{ good, neutral, bad, all, average }} />
     </>
   );
 };

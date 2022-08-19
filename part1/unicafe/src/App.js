3ed86682398ca40951f33a9a24f124c3d4d7243b
Leandro Reis <leandro.reiz@gmail.com>
2022-08-19 23:19:20 +0100

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,16 +2,29 @@ import { useState } from 'react';
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Display = ({ value, text }) => <div>{`${text} ${value}`}</div>;
-
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
 
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setNeutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
+  const handleGood = () => {
+    setAll(all + 1);
+    setGood(good + 1);
+    setAverage(average + 1);
+  };
+
+  const handleNeutral = () => {
+    setAll(all + 1);
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    setAll(all + 1);
+    setBad(bad + 1);
+    setAverage(average - 1);
+  };
 
   return (
     <>
@@ -21,9 +34,12 @@ const App = () => {
       <Button onClick={handleBad} text="bad" />
 
       <h1>statistics</h1>
-      <Display value={good} text="good" />
-      <Display value={neutral} text="neutral" />
-      <Display value={bad} text="bad" />
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average ? average / all : 0}</div>
+      <div>positive {good ? (good / all) * 100 : 0} %</div>
     </>
   );
 };

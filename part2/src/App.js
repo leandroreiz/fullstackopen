f4a6b485c6f27f11b0d331c92fe84bab236763ee
Leandro Reis <leandro.reiz@gmail.com>
2022-08-22 20:21:21 +0100

    Adding styles to React app

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react';
 
 import noteService from './services/notes';
 import Note from './components/Note';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
@@ -26,7 +29,12 @@ const App = () => {
         setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)));
       })
       .catch((error) => {
-        alert(`the note ${note.content} was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -52,6 +60,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -70,6 +79,8 @@ const App = () => {
         <input onChange={handleNoteChange} value={newNote} />
         <button type="submit">save</button>
       </form>
+
+      <Footer />
     </div>
   );
 };

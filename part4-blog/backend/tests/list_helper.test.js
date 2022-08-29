a3082ec61542c891d56a9a40012885746e92000f
Leandro Reis <leandro.reiz@gmail.com>
2022-08-29 20:52:33 +0100

    Exercises 4.5*-4.7*

--- part4-blog/backend/tests/list_helper.test.js
+++ part4-blog/backend/tests/list_helper.test.js
@@ -1,43 +1,103 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy return one', () => {
-  const blogs = []
+const emptyList = []
 
-  const result = listHelper.dummy(blogs)
+const listWithOneBlog = [
+  {
+    title: 'REST API Design Best Practices Handbook',
+    author: 'Jean-Marc Möckel',
+    url: 'https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/',
+    likes: 22,
+  }
+]
+
+const biggerList = [
+  {
+    title: 'REST API Design Best Practices Handbook',
+    author: 'Jean-Marc Möckel',
+    url: 'https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/',
+    likes: 22,
+  },
+  {
+    title: 'Git Commit Best Practices',
+    author: 'Luis Matos',
+    url: 'https://gist.github.com/luismts/495d982e8c5b1a0ced4a57cf3d93cf60',
+    likes: 999,
+  },
+  {
+    title: 'Statements Vs. Expressions',
+    author: 'Josh Comeau',
+    url: 'https://www.joshwcomeau.com/javascript/statements-vs-expressions/',
+    likes: 45,
+  },
+  {
+    title: 'Regular Expressions Cheat Sheet',
+    author: 'Dave Child',
+    url: 'https://cheatography.com/davechild/cheat-sheets/regular-expressions/',
+    likes: 33,
+  },
+  {
+    title: 'Clean Code Best Practices',
+    author: 'Luis Matos',
+    url: 'https://gist.github.com/luismts/495d982e8c5b1a0ced4a57cf3d93cf6e',
+    likes: 1,
+  },
+  {
+    title: 'React Best Practices',
+    author: 'Jean-Marc Möckel',
+    url: 'https://www.freecodecamp.org/news/best-practices-for-react/',
+    likes: 3,
+  },
+  {
+    title: 'Healthy Habits',
+    author: 'Jean-Marc Möckel',
+    url: 'https://www.freecodecamp.org/news/how-to-become-a-better-developer-and-live-a-happier-life/',
+    likes: 7,
+  },
+  {
+    title: 'The Surprising Truth About Pixels and Accessibility',
+    author: 'Josh Comeau',
+    url: 'https://www.joshwcomeau.com/css/surprising-truth-about-pixels-and-accessibility/',
+    likes: 26,
+  },
+  {
+    title: 'How I Landed My First Developer Job Without Writing a Single Application',
+    author: 'Jean-Marc Möckel',
+    url: 'https://www.freecodecamp.org/news/how-i-landed-my-first-developer-job-without-an-application/',
+    likes: 17,
+  },
+  {
+    title: 'Why React Re-Renders',
+    author: 'Josh Comeau',
+    url: 'https://www.joshwcomeau.com/react/why-react-re-renders/',
+    likes: 34,
+  },
+  {
+    title: 'Testing TypeScript apps using Jest',
+    author: 'Ibiyemi Adewakun',
+    url: 'https://blog.logrocket.com/testing-typescript-apps-using-jest/',
+    likes: 5,
+  },
+  {
+    title: 'My Wonderful HTML Email Workflow',
+    author: 'Josh Comeau',
+    url: 'https://www.joshwcomeau.com/react/wonderful-emails-with-mjml-and-mdx/',
+    likes: 51,
+  },
+  {
+    title: 'You Don\'t Need A UI Framework',
+    author: 'Josh Comeau',
+    url: 'https://www.smashingmagazine.com/2022/05/you-dont-need-ui-framework/',
+    likes: 14,
+  }
+]
+
+test('dummy return one', () => {
+  const result = listHelper.dummy(emptyList)
   expect(result).toBe(1)
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '630bbb555412809a9e4bbd0d',
-      title: 'REST API Design Best Practices Handbook',
-      author: 'Jean-Marc Möckel',
-      url: 'https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/',
-      likes: 22,
-      __v: 0
-    }
-  ]
-
-  const biggerList = [
-    {
-      _id: '630bbb555412809a9e4bbd0d',
-      title: 'REST API Design Best Practices Handbook',
-      author: 'Jean-Marc Möckel',
-      url: 'https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/',
-      likes: 22,
-      __v: 0
-    },
-    {
-      _id: '630bbbaa5412809a9e4bbd10',
-      title: 'Git Commit Best Practices',
-      author: 'Luis Matos',
-      url: 'https://gist.github.com/luismts/495d982e8c5b1a0ced4a57cf3d93cf60',
-      likes: 99,
-      __v: 0
-    }
-  ]
-
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([])
     expect(result).toBe(0)
@@ -50,6 +110,38 @@ describe('total likes', () => {
 
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(biggerList)
-    expect(result).toBe(121)
+    expect(result).toBe(1257)
+  })
+})
+
+describe('favorite blog', () => {
+  test('return the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(biggerList)
+    expect(result).toEqual({
+      title: 'Git Commit Best Practices',
+      author: 'Luis Matos',
+      url: 'https://gist.github.com/luismts/495d982e8c5b1a0ced4a57cf3d93cf60',
+      likes: 999,
+    })
+  })
+})
+
+describe('most common author', () => {
+  test('return the author that has more blogs saved', () => {
+    const result = listHelper.mostBlogs(biggerList)
+    expect(result).toEqual({
+      author: 'Josh Comeau',
+      blogs: 5
+    })
+  })
+})
+
+describe('most liked author', () => {
+  test('return the author with the most likes among all blogs', () => {
+    const result = listHelper.mostLikes(biggerList)
+    expect(result).toEqual({
+      author: 'Luis Matos',
+      likes: 1000
+    })
   })
 })
--- part4-blog/backend/utils/list_helper.js
+++ part4-blog/backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   console.log(blogs)
   return 1
@@ -7,7 +9,61 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => sum + item.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let favorite = -1
+  let max = 0
+
+  blogs.forEach((blog, idx) => {
+    if (blog.likes > max) {
+      max = blog.likes
+      favorite = idx
+    }
+  })
+
+  return blogs[favorite]
+}
+
+const mostBlogs = (blogs) => {
+  // creates an array with the authors
+  const authorsList = _.map(blogs, 'author')
+
+  const mostBlogs = _
+    .chain(authorsList)
+    .countBy()
+    .toPairs()
+    .maxBy(_.last)
+    .value()
+  return {
+    author: mostBlogs[0],
+    blogs: mostBlogs[1]
+  }
+}
+
+const mostLikes = (blogs) => {
+  const list = blogs
+    .reduce((blog, { author, likes }) => {
+      blog[author] = blog[author] || 0
+      blog[author] += likes
+
+      return blog
+    }, {})
+
+  const mostLiked = _
+    .chain(list)
+    .toPairs()
+    .maxBy(_.last)
+    .value()
+
+  return {
+    author: mostLiked[0],
+    likes: mostLiked[1]
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
